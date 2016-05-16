82b1a31768fb5861ebd3612f8950a379c805f0aa
Amjed Hallak <ahallak@udel.edu>
2016-05-16 14:38:41 -0400

    BLL done - no unit tests yet

--- RocketBLL/src/main/java/exceptions/RateException.java
+++ RocketBLL/src/main/java/exceptions/RateException.java
@@ -1,9 +1,22 @@
 package exceptions;
 
+import rocketDomain.RateDomainModel;
+
 public class RateException extends Exception {
 
 	//	TODO - RocketBLL RateException - RateDomainModel should be an attribute of RateException
-	//	* Add RateRomainModel as an attribute
+	//	* Add RateDomainModel as an attribute
 	//	* Create a constructor, passing in RateDomainModel
 	//	* Create a getter (no setter, set value only in Constructor)
+	// * DONE *
+	private RateDomainModel rateDomainModel = null;
+
+	public RateException(RateDomainModel rateDomainModel) {
+		this.rateDomainModel = rateDomainModel;
+	}
+
+	public RateDomainModel getRateDomainModel() {
+		return rateDomainModel;
+	}
+
 }
--- RocketBLL/src/main/java/rocketBase/RateBLL.java
+++ RocketBLL/src/main/java/rocketBase/RateBLL.java
@@ -1,36 +1,47 @@
 package rocketBase;
 
+import java.util.ArrayList;
+
 import org.apache.poi.ss.formula.functions.*;
+
+import exceptions.RateException;
+import rocketDomain.RateDomainModel;
 
 public class RateBLL {
 
 	private static RateDAL _RateDAL = new RateDAL();
-	
-	static double getRate(int GivenCreditScore) 
-	{
-		//TODO - RocketBLL RateBLL.getRate - make sure you throw any exception
-		
-		//		Call RateDAL.getAllRates... this returns an array of rates
-		//		write the code that will search the rates to determine the 
-		//		interest rate for the given credit score
-		//		hints:  you have to sort the rates...  you can do this by using
-		//			a comparator... or by using an OrderBy statement in the HQL
-		
-		
-		//TODO - RocketBLL RateBLL.getRate
-		//			obviously this should be changed to return the determined rate
-		return 0;
-		
-		
+
+	static double getRate(int GivenCreditScore) throws RateException {
+		// TODO - RocketBLL RateBLL.getRate - make sure you throw any exception
+		// Call RateDAL.getAllRates... this returns an array of rates
+		// write the code that will search the rates to determine the
+		// interest rate for the given credit score
+		// hints: you have to sort the rates... you can do this by using
+		// a comparator... or by using an OrderBy statement in the HQL
+		ArrayList<RateDomainModel> listRates = _RateDAL.getAllRates();
+		double rateDouble = -1;
+		RateDomainModel rdmRt = null;
+		for (RateDomainModel rate : listRates) {
+			if (rate.getiMinCreditScore() == GivenCreditScore) {
+				rateDouble = rate.getdInterestRate();
+				rdmRt = rate;
+			}
+		}
+
+		// TODO - RocketBLL RateBLL.getRate
+		// obviously this should be changed to return the determined rate
+		if (rdmRt == null) {
+			throw new RateException(rdmRt);
+		} else {
+			return rateDouble;
+		}
 	}
-	
-	
-	//TODO - RocketBLL RateBLL.getPayment 
-	//		how to use:
-	//		https://poi.apache.org/apidocs/org/apache/poi/ss/formula/functions/FinanceLib.html
-	
-	public static double getPayment(double r, double n, double p, double f, boolean t)
-	{		
-		return FinanceLib.pmt(r, n, p, f, t);
+
+	// TODO - RocketBLL RateBLL.getPayment
+	// how to use:
+	// https://poi.apache.org/apidocs/org/apache/poi/ss/formula/functions/FinanceLib.html
+
+	public static double getPayment(double r, double n, double p, double f, boolean t) {
+		return -FinanceLib.pmt(r, n, p, f, t);
 	}
 }
